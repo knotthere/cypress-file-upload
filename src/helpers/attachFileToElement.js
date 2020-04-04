9db681f55b41e8c87778bdd76088df5a2654a852
knottl <larry.knott@autodesk.com>
2020-04-04 13:21:07 -0700

    Fix file drag-n-drop for Webkit: provide DataTransferItem.webkitGetAsEntry()

--- src/helpers/attachFileToElement.js
+++ src/helpers/attachFileToElement.js
@@ -2,7 +2,17 @@ import { SUBJECT_TYPE, EVENTS_BY_SUBJECT_TYPE } from '../constants';
 
 export default function(subject, { file, subjectType, force }) {
   const dataTransfer = new DataTransfer();
-  dataTransfer.items.add(file);
+  const dataTransferItem = dataTransfer.items.add(file);
+
+  // https://github.com/silverwind/uppie/pull/11
+  if (subjectType === SUBJECT_TYPE.DRAG_N_DROP) {
+    dataTransferItem.webkitGetAsEntry = () => {
+      return {
+        isFile: true,
+        file: successCallback => successCallback(file),
+      };
+    };
+  }
 
   if (subjectType === SUBJECT_TYPE.INPUT) {
     const inputElement = subject[0];
